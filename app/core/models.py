53f4f4e41f25436feba4862f9fcbf7557e55105e
nakashi120 <nakashi120@gmail.com>
2022-11-18 00:41:49 +0700

    Modified User model

--- app/core/models.py
+++ app/core/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
-    PermissionsMixin
+    PermissionsMixin,
 )
 
 
@@ -27,5 +27,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
 
     USERNAME_FIELD = 'email'
