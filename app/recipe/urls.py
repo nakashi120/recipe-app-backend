74ced5c149c1f495034602c417fbc2d565b761b3
nakashi120 <nakashi120@gmail.com>
2022-11-24 23:43:21 +0700

    Implement tag listing API

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -10,9 +10,9 @@ from rest_framework.routers import DefaultRouter
 
 from recipe import views
 
-
 router = DefaultRouter()
 router.register('recipes', views.RecipeViewSet)
+router.register('tags', views.TagViewSet)
 
 app_name = 'recipe'
 
