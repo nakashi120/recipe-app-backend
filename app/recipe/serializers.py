4a8d8506381e6ea81f0e510a315c6b5d8e7d5e04
nakashi120 <nakashi120@gmail.com>
2022-11-30 01:58:49 +0700

    Configured project for static files

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -71,14 +71,18 @@ class RecipeSerializer(serializers.ModelSerializer):
 
         return recipe
 
-    def update(self, instance, validate_data):
+    def update(self, instance, validated_data):
         """Update a recipe."""
-        tags = validate_data.pop('tags', None)
+        tags = validated_data.pop('tags', None)
+        ingredients = validated_data.pop('ingredients', None)
         if tags is not None:
             instance.tags.clear()
             self._get_or_create_tags(tags, instance)
+        if ingredients is not None:
+            instance.ingredients.clear()
+            self._get_or_create_ingredients(ingredients, instance)
 
-        for attr, value in validate_data.items():
+        for attr, value in validated_data.items():
             setattr(instance, attr, value)
 
         instance.save()
